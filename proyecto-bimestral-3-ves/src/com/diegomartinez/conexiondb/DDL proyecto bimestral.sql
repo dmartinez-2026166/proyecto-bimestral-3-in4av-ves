02c00b757c0b6b5ff379b813111d40c235c23d5d
Diego Martinez <dmartinez-2026166@kinal.edu.gt>
2026-07-21 21:15:41 -0600

    Se agregó el archivo .sql para relacionar la base de datos

--- proyecto-bimestral-3-ves/src/com/diegomartinez/conexiondb/DDL proyecto bimestral.sql
+++ proyecto-bimestral-3-ves/src/com/diegomartinez/conexiondb/DDL proyecto bimestral.sql
@@ -1,19 +1,30 @@
-create database if not exists proyecto_b3_2026166_in4av;
+drop database if exists proyecto_b3_2026166_in4av;
+create database proyecto_b3_2026166_in4av;
 use proyecto_b3_2026166_in4av;
 
-Create table usuario (
+create table usuario (
 	id_usuario INT auto_increment primary key,
     nombre_completo VARCHAR(100) not null,
     usuario varchar(50) not null UNIQUE,
     correo varchar(100) not null unique,
     clave varchar(255) not null
 );
+ 
+delimiter $$
+	create procedure sp_crear_usuario(
+			IN p_nombre_completo VARCHAR(100),
+			IN p_usuario VARCHAR(50),
+			IN p_correo VARCHAR(100),
+			IN p_clave VARCHAR(255))
+	begin
+		insert into usuario (nombre_completo, usuario, correo, clave)
+			values (p_nombre_completo, p_usuario, p_correo, p_clave);
+	end $$
+delimiter ;
 
 delimiter $$
-	create procedure sp_crear_usuario()
-    
+	create procedure sp_leer_usuario()
     begin
-		insert into usuario()
-			values(uuid());
+		select * from usuario;
     end $$
 delimiter ;
